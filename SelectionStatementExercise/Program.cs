d1cd47527761632c09a3f7b3c712258773b0540a
Stephen-May1_Dell <stephen_may1@dell.com>
2024-07-08 13:27:59 -0400

    exercise 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            Exercise1();
+            Exercise2();    
+        }
+
+        public static void Exercise1()
+        {
             var r = new Random();
             var favNumber = r.Next(1, 1000);
 
@@ -23,6 +29,40 @@
             {
                 Console.WriteLine("You guessed it! Congratulations!");
             }
+        }
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+            favoriteSubject = favoriteSubject.ToLower();
+            switch (favoriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math is great for problem-solving and logic!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is fascinating and helps us understand the world around us!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is the language of literature and communication!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History teaches us about the past and helps us understand the present!");
+                    break;
+
+                case "music":
+                    Console.WriteLine("Music is the language of emotions and brings joy to our lives!");
+                    break;
+
+                default:
+                    Console.WriteLine("That's an interesting choice! I'm glad you enjoy your favorite subject!");
+                    break;
+            }
+
         }
     }
 }
