91a174fcfb4d4c85cfea53d75e1e0b2be216db7d
Stephen-May1_Dell <stephen_may1@dell.com>
2024-07-08 13:24:08 -0400

    exercise completion

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Guess my favorite number between 1 and 1000:");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! Congratulations!");
+            }
         }
     }
 }
